7f55f83a8189135155a2d34edb7fb23c87aa2e38
Don <71207900+VaperinaDEV@users.noreply.github.com>
2024-11-09 16:33:00 +0100

    Create code-block-line-numbers.js

---
+++ javascripts/discourse/api-initializers/code-block-line-numbers.js
@@ -0,0 +1,27 @@
+import { apiInitializer } from "discourse/lib/api";
+
+export default apiInitializer("1.14.0", (api) => {
+  const decorateLines = (post) => {
+    try {
+      const classes = ['decorator', 'number', 'wrap', 'scroll'].map(c => `lines-${c}`);
+  
+      const split = /^(.*)$/mg;
+      const elems = post.querySelectorAll('pre:not(.onebox) code:not(.lines-decorator)');
+      elems.forEach(elem => {
+        const count = elem.innerHTML.trim().match(split).length;
+        const quote = elem.closest('aside') ? ['lines-in-quote'] : []
+  
+        elem.parentElement.classList.add(`lines-count-${count}`, ...classes, ...quote);
+        elem.classList.add(`lines-count-${count}`, ...classes, ...quote);
+        const lineClass = ['lines-line', ...quote].join(' ');
+  
+    // trim to hide trailing blank lines
+        elem.innerHTML = elem.innerHTML.trim().replace(/^(.*)$/mg, `<span class="${lineClass}">$1</span>`);
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  
+  api.decorateCookedElement(decorateLines, {id: 'decorate-pre-code-lines'});
+});
